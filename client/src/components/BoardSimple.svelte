8717f8e83d3d3a939087c29448dc90627f2393e5
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2023-04-10 19:00:17 -0700

    prettier

--- client/src/components/BoardSimple.svelte
+++ client/src/components/BoardSimple.svelte
@@ -1,71 +1,151 @@
 <script>
-  import {reverseList} from '../helpers/helpers'
+	import { reverseList } from '../helpers/helpers';
 
-  export const boardState = ['1','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',]
-  const fileCoords = [1,2,3,4,5,6,7,8,9]
-  const rankCoords = ['a','b','c','d','e','f','g','h','i']
+	export const boardState = [
+		'1',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+		'',
+	];
+	const fileCoords = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	const rankCoords = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
 </script>
 
-<div class='board'>
-  {#each boardState as square}
-    <div class='square'>
-      {#if square}
-        <img class='piece' src="/pieces/b1中.svg" alt="">
-      {/if}
-    </div>
-  {/each}
+<div class="board">
+	{#each boardState as square}
+		<div class="square">
+			{#if square}
+				<img class="piece" src="/pieces/b1中.svg" alt="" />
+			{/if}
+		</div>
+	{/each}
 
-  <div class='file'>
-    {#each reverseList(fileCoords) as file}
-      <div class=''>{file}</div>
-    {/each}
-  </div>
+	<div class="file">
+		{#each reverseList(fileCoords) as file}
+			<div class="">{file}</div>
+		{/each}
+	</div>
 
-  <div class='rank'>
-    {#each rankCoords as rank}
-      <div class=''>{rank}</div>
-    {/each}
-  </div>
+	<div class="rank">
+		{#each rankCoords as rank}
+			<div class="">{rank}</div>
+		{/each}
+	</div>
 </div>
 
 <style>
+	.board {
+		display: grid;
+		grid-template-columns: repeat(9, minmax(20px, 1fr));
+		grid-template-rows: repeat(9, minmax(20px, 1fr));
+		gap: 2px;
+		max-width: 45rem;
+		aspect-ratio: 1/1;
+		background-color: rgb(226, 147, 49);
+		padding: 2px;
+		margin: 2rem;
+		position: relative;
+	}
 
-  .board{
-    display: grid;
-    grid-template-columns: repeat(9, minmax(20px, 1fr));
-    grid-template-rows: repeat(9, minmax(20px, 1fr));
-    gap:2px;
-    max-width: 45rem;
-    aspect-ratio: 1/1;
-    background-color:rgb(226, 147, 49);
-    padding:2px;
-    margin: 2rem;
-    position:relative;
-  }
+	.square {
+		background-color: rgb(254 215 170);
+	}
 
-  .square{
-    background-color:rgb(254 215 170);
-  }
+	.file {
+		display: grid;
+		position: absolute;
+		height: 100%;
+		margin-left: -1rem;
+		align-items: center;
+	}
 
-  .file{
-    display: grid;
-    position: absolute;
-    height: 100%;
-    margin-left: -1rem;
-    align-items: center;
-  }
+	.rank {
+		display: grid;
+		position: absolute;
+		grid-auto-flow: column;
+		width: 100%;
+		bottom: -1.5rem;
+		text-align: center;
+	}
 
-  .rank{
-    display: grid;
-    position: absolute;
-    grid-auto-flow: column;
-    width: 100%;
-    bottom: -1.5rem;
-    text-align: center;
-  }
-
-  .piece{
-    padding:.375rem;
-  }
-
+	.piece {
+		padding: 0.375rem;
+	}
 </style>
--- client/src/components/Navbar.svelte
+++ client/src/components/Navbar.svelte
@@ -1,21 +1,22 @@
-<nav class='navbar'>
-  <a href="/">Gungi.go</a>
-  <ul class='nav-list'>
-    <a href="/play">play</a>
-    <a href="/login">login</a>
-  </ul>
+<nav class="navbar">
+	<a href="/">Gungi.go</a>
+	<ul class="nav-list">
+		<a href="/play">play</a>
+		<a href="/login">login</a>
+		<a href="/register">register</a>
+	</ul>
 </nav>
 
 <style>
-  .navbar{
-    display:flex;
-    justify-content: space-between;
-    /* background-color:red; */
-    /* max-width: 1000px; */
-    padding: .5rem 12rem;
-  }
-  .nav-list{
-    display:flex;
-    gap:1rem
-  }
+	.navbar {
+		display: flex;
+		justify-content: space-between;
+		/* background-color:red; */
+		/* max-width: 1000px; */
+		padding: 0.5rem 12rem;
+	}
+	.nav-list {
+		display: flex;
+		gap: 1rem;
+	}
 </style>
