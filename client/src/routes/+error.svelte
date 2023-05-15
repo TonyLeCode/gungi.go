9435bb52a817bca4f6881d94b2775626fd9ae92f
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2023-05-14 17:41:11 -0700

    Added 404

---
+++ client/src/routes/+error.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { page } from '$app/stores';
+  console.log($page.error)
+</script>
+
+<main>
+	<h1>{$page.status == 404 ? 'Page Not Found' : 'Server Error'}</h1>
+	{#if $page.status == 404}
+		<p>Looks like you're lost. Go back <a href='/'>home</a></p>
+	{:else}
+		<p>Looks like something went wrong in the server. Try again later.</p>
+	{/if}
+</main>
+
+<style>
+	main {
+		max-width: 70rem;
+		margin: auto;
+		text-align: center;
+		font-size: 1.5rem;
+		min-height: calc(100vh - 3rem);
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+	}
+	h1 {
+		font-size: 2rem;
+		font-weight: 600;
+	}
+	p {
+		margin-bottom: 12rem;
+	}
+  a{
+    color: rgb(255, 94, 0);
+  }
+</style>
