ffe5586b3512422d3db9844af569efd967e35bc6
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2023-10-27 18:28:50 -0700

    Changed overview to sort games

--- client/src/routes/overview/+page.svelte
+++ client/src/routes/overview/+page.svelte
@@ -3,24 +3,40 @@
 	import type {Game} from './+page.server'
 
 	export let data;
+	const username = data.session?.user.user_metadata.username ?? ''
+	$: sortedGames = [...data.data].sort((a, b) => {
+		if (turnPlayer(a) === turnPlayer(b)){
+			return 0
+		}
+		if (isUserTurn(a)){
+			return -1
+		} else if (isUserTurn(b)){
+			return 1
+		}
+		return 0
+	})
+
+	function isUser(playername: string){
+		return username === playername
+	}
 
 	function getUserColor(player1: string, player2: string){
-		const user = data.session?.user.user_metadata.username ?? '';
-		if (user === player1) {
+		if (username === player1) {
 			return 'w'
-		} else if (user === player2){
+		} else if (username === player2){
 			return 'b'
 		}
 		return 'spectator'
 	}
 
-	function isUserTurn(game: Game) {
+	function turnPlayer(game: Game){
 		const fields = game.current_state.split(' ');
 		const turnColor = fields[2];
-		const turnPlayer = () => {
-			return turnColor === 'w' ? game.username1 : game.username2;
-		};
-		return turnPlayer() === data.session?.user.user_metadata.username;
+		return turnColor === 'w' ? game.username1 : game.username2;
+	}
+
+	function isUserTurn(game: Game) {
+		return turnPlayer(game) === username
 		// if(data.session){
 		// 	return data.session.user.user_metadata.username === user
 		// } else {
@@ -40,11 +56,11 @@
 	<section>
 		<h2>Current Games</h2>
 		<ul class="gameList">
-			{#each data.data as game}
+			{#each sortedGames as game}
 				<li class:your-turn={isUserTurn(game)}>
-					<div class="name name-1">{game.username1}</div>
-					<a href={`/game/${game.id}`}><BoardSimple gameData={game} isUserTurn={isUserTurn(game)} userColor={getUserColor(game.username1, game.username2)} /></a>
-					<div class="name name-2">{game.username2}</div>
+					<div class="name name-1" class:is-user={isUser(game.username1)}>{game.username1}</div>
+					<a href={`/game/${game.id}`}><BoardSimple gameData={game} userColor={getUserColor(game.username1, game.username2)} /></a>
+					<div class="name name-2" class:is-user={isUser(game.username2)}>{game.username2}</div>
 				</li>
 			{/each}
 			<!-- <BoardSimple gameData={{current_state:'3,k,5/3,psc,m,4/5,pyt,3/9/9/9/5,PT,P,2/6,F,PS,1/7,M,1 6446122122210/6446212121210 w'}} />
@@ -77,12 +93,13 @@
 	}
 
 	li:hover {
-		outline: rgb(240, 80, 17) solid 4px;
+		outline: rgb(240, 80, 17) solid 6px;
 		/* box-sizing: content-box; */
 	}
 
 	.your-turn {
-		outline: rgba(255, 136, 81, 0.829) solid 2px;
+		/* outline: rgba(255, 136, 81, 0.829) solid 2px; */
+		outline: rgb(240, 80, 17) solid 4px;
 	}
 
 	.gameList {
@@ -110,5 +127,9 @@
 	.name-2 {
 		right: 0;
 		top: -1.5rem;
+	}
+	.is-user {
+		color: rgb(var(--primary));
+		/* font-weight: 600; */
 	}
 </style>
