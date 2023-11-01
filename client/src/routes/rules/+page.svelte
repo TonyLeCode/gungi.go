b73bb61d0c4fcfd6b78ef1c78dd23a1522d23cd5
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2023-10-31 18:46:48 -0700

    Added rules page

---
+++ client/src/routes/rules/+page.svelte
@@ -0,0 +1,16 @@
+
+<main>
+  <h1>This page does not yet exist</h1>
+</main>
+
+<style lang='scss'>
+  main{
+		max-width: 70rem;
+		margin:auto;
+	}
+  h1 {
+    font-size: 2rem;
+    text-align: center;
+    margin-top: 2rem;
+  }
+</style>
