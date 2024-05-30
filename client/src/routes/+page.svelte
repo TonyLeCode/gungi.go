de485423e0e9fa201257940af798182665b154a9
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2024-05-29 22:10:02 -0700

    Update home page

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -4,22 +4,37 @@
 
 <svelte:head>
 	<title>White Monarch Server</title>
+	<meta
+		name="description"
+		content="White Monarch Server is a server where you can play Gungi online against other players. Gungi is a fictional strategy game in Hunter x Hunter, made by Yoshihiro Togashi."
+	/>
 </svelte:head>
 
 <main>
-	<h1>White Monarch Server</h1>
-	<img src="/home-splash.png" alt="home-splash">
-	<!-- <p>Welcome! This is a free web app to play Gungi online. You can spectate or play live and correspondence games.</p> -->
-	<p>"You should enjoy the little detours to the fullest. Because that's where you'll find things more important than what you want."</p>
-	<p class="author">- Ging Freecss</p>
-	{#if data.session}
-		<a class="button-primary" href="/play">Play</a>
-	{:else}
-		<div>
-			<a class="button-primary" href="/login">Login</a>
-			<a class="button-primary" href="/register">Register</a>
-		</div>
-	{/if}
+	<section class="intro">
+		<h1>White Monarch Server</h1>
+		<img src="/home-splash.png" alt="home-splash" />
+		<!-- <p>Welcome! This is a free web app to play Gungi online. You can spectate or play live and correspondence games.</p> -->
+		<p>
+			"You should enjoy the little detours to the fullest. Because that's where you'll find things more important than
+			what you want."
+		</p>
+		<p class="author">- Ging Freecss</p>
+		{#if data.session}
+			<a class="button-primary" href="/play">Play</a>
+		{:else}
+			<div>
+				<a class="button-primary" href="/login">Login</a>
+				<a class="button-primary" href="/register">Register</a>
+			</div>
+		{/if}
+	</section>
+	<section class="description">
+		<h2>Gungi</h2>
+		<p>Gungi is a fictional strategy game in Hunter x Hunter, made by Yoshihiro Togashi. It is similar to Chess or Shogi except players can create their own formations at the start of the game, and pieces may be stacked onto each other.</p>
+		<p>Since the game is unofficial, there are various rulesets, currently we only have the revised community ruleset.</p>
+		<p>I've created this site to provide a platform for players to play correspondence games against each other. I hope you enjoy your stay.</p>
+	</section>
 </main>
 
 <style>
@@ -30,8 +45,19 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		min-height: 50vh;
 		padding: clamp(0.75rem, 2vw, 2rem);
+	}
+
+	.intro {
+		margin: auto;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-bottom: clamp(4rem, 15vw, 14rem);
+	}
+
+	.description {
+		margin-bottom: 20rem;
 	}
 
 	h1 {
@@ -43,6 +69,13 @@
 		@media (min-width: 767px) {
 			font-size: 2.5rem;
 		}
+	}
+	h2 {
+		font-size: clamp(1.25rem, 5vw, 2rem);
+		text-align: center;
+		font-weight: 600;
+		margin-top: 1rem;
+		/* letter-spacing: .1rem; */
 	}
 	p {
 		margin-top: 1rem;
@@ -72,7 +105,7 @@
 		}
 	}
 
-	img{
+	img {
 		margin: clamp(1rem, 2vw, 2rem);
 	}
 </style>
